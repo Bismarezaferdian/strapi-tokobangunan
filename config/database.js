908a42663623878d12f4d05a94a07d08b9901a81
Bisma Reza Ferdian <bismarezaferdian95@gmail.com>
2024-01-10 22:17:33 +0700

    update database config

--- config/database.js
+++ config/database.js
@@ -104,10 +104,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "megautama"),
-      user: env("DATABASE_USERNAME", "bisma"),
+      host: env("DATABASE_HOST", "db.ghemsayisbshzkyeywxj.supabase.co"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "Bismarez0506"),
       ssl: env.bool("DATABASE_SSL", false),
     },
